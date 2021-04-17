a6767ae8bdab8d8b78e18c98c4e052dda0f203f2
Ivan <78632369+Ivan-MLN@users.noreply.github.com>
2021-04-17 14:04:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ apt-get install nodejs -y
 apt-get install ffmpeg libwebp -y
 apt-get install wget -y
 apt-get install tesseract -y
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm i node-tesseract-ocr
+npm i cheerio
 npm i
-npm audit fix
 npm start
