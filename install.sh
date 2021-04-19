08ac9c833f913c29aac50f7fd38f6a08d5b07dc6
Ivan <78632369+Ivan-MLN@users.noreply.github.com>
2021-04-19 13:55:40 +0700

    @Ivanzz

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ apt-get upgrade
 apt-get install nodejs -y
 apt-get install ffmpeg libwebp -y
 apt-get install wget -y
+apt-get install imagemagick -y
 apt-get install tesseract -y
 npm i node-tesseract-ocr
 npm i cheerio
